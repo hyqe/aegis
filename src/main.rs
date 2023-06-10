fc23b2e1d75788a70f0dc468ff46f9ad0251d7af
LukeOchoa <Luke.Ochoa@gmail.com>
2023-06-09 21:06:33 -0700

    Add basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod server;
+
+#[tokio::main]
+async fn main() {
+    let cfg = server::Config::get_config().unwrap();
+
+    println!("Server running on port: {}", cfg.port);
+    server::run(cfg).await;
 }
